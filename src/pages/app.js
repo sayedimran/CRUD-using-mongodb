d3052b01a35d4bec95cc2996f373fd3f48fe2169
Imran Sayed <sayedimrankashmiri@gmail.com>
2020-03-05 23:23:26 +0530

    Home and App Routes Page

--- src/pages/app.js
+++ src/pages/app.js
@@ -4,7 +4,10 @@ import Layout from "../components/Layout"
 import Login from "../components/Login"
 import Register from "../components/Register"
 import PrivateRoute from "../components/PrivateRoutes"
+
 import Dashboard from "../components/Dashboard"
+import Product from "../components/Product"
+import Addnew from "../components/Addnew"
 
 const app = () => {
   return (
@@ -13,6 +16,8 @@ const app = () => {
         <Register path="/app/register" />
         <Login path="/app/login" />
         <PrivateRoute path="/app/dashboard" component={Dashboard} />
+        <Product path="/app/product/:id" />
+        <Addnew path="/app/product/new" />
       </Router>
     </Layout>
   )
