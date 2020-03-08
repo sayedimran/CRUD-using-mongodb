02bdbeb2ff7d6c0c4d6791364587dd3d612c1442
Imran Sayed <sayedimrankashmiri@gmail.com>
2020-03-08 15:57:08 +0530

    Welcome: If user logged in render dashboard else welcome

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,15 @@
 import React from "react"
 import Welcome from "../components/Welcome"
 import Layout from "../components/Layout"
+import { navigate } from "gatsby"
 
 const IndexPage = () => {
+  const user = localStorage.getItem("token")
+  console.log(user)
+  if (user) {
+    navigate("/app/dashboard")
+  }
+
   return (
     <Layout>
       <Welcome />
